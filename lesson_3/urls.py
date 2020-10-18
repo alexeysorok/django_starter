e2e4cc2ad105036d0f2e906acf80eaf7b02176d3
alexe <alexe@W10SSD>
2020-10-19 00:21:33 +0300

    Add: Template TemplateView

--- lesson_3/urls.py
+++ lesson_3/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, post_view
 
 
 urlpatterns = [
@@ -10,6 +10,9 @@ urlpatterns = [
     path("main/not-allowed/", views.not_allowed, name="not_allowed"),
     path("main/json/", views.json, name="json"),
 
-    path('class-view/', views.MyView.as_view(), name='class_view'),
-    path('class-view/text', views.MyView.as_view(), name='class_view_text'),
+    # path('class-view/', views.MyView.as_view(), name='class_view'),
+    # path('class-view/text', views.MyView.as_view(), name='class_view_text'),
+
+    path('post/', post_view.MyTemplateView.as_view(), name="post"),
+    path('post/<int:number>/', post_view.post_page, name="post_list"),
 ]
